993f46b94e68afbb5fea10df75dd15570979d39f
karthik-d <karthikdesingu2000@gmail.com>
2022-08-11 19:37:21 +0530

    sample interfaces for date

--- task.c
+++ task.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define STD_STRING_SIZE 30
 
-/* Structure and Interface to handle date */
+/* Structure and Interface(s) to handle date */
 
 struct date {
     int day;
@@ -13,7 +14,31 @@ struct date {
 typedef struct date Date;
 
 
+Date* initialize_date(int day, int month, int year) {
+    Date *container = (Date*)malloc(sizeof(Date));
+    *(container) = { day, month, year };
+    return container;
+}
 
+
+Date* get_current_local_date() {
+    time_t periodic_time = time(NULL);
+    struct tm* formatted_time = localtime(&periodic_time);
+
+    Date *local_date = initialize_date(
+        formatted_time->tm_mday;
+        formatted_time->tm_mon;
+        formatted_time->tm_year;
+    );
+    return local_date;
+}
+
+
+char* get_printable_date(Date *date){
+    char *date_str = (char*)malloc(sizeof(char)*STD_STRING_SIZE);
+    sprintf(date_str, "%4d-%2d-%2d", year, month, day);
+    return date_str;
+}
 
 
 /* ---------- */
@@ -36,5 +61,6 @@ typedef struct completed_task CompletedTask;
 int main(int argc, char* argv[])
 {
     printf("Hello, World!");
+    printf("\nToday is: %s\n", get_printable_date(get_current_local_date));
     return 0;
 }
